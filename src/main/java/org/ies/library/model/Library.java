2a59ba33335b779e97c8499f885164925293ae41
Carol <carol@b25>
2025-03-11 12:39:39 +0100

    Métodos

--- src/main/java/org/ies/library/model/Library.java
+++ src/main/java/org/ies/library/model/Library.java
@@ -2,10 +2,9 @@ package org.ies.library.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.w3c.dom.ls.LSException;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 
 @Data
  @AllArgsConstructor
@@ -15,6 +14,36 @@ public class Library {
     private Map<String, Book> booksByIsbn;
     private List<Customer> customers;
     private TreeSet<BookLend> bookLends;
+
+    // //Dado un género, devuelve todos los libros que tengan ese género (List<Book>).
+    public List<Book> listGenreBooks (String genres){
+        List<Book> genreBooks = new ArrayList<>();
+        for (Book book : booksByIsbn.values()){
+            if(book.getGenres().contains(genres)){
+                genreBooks.add(book);
+            }
+        }
+        return genreBooks;
+    }
+
+    // Dado un código postal, devuelve los socios que viven ahí (List<Customer>)
+    public List<Customer> customersByZipCode (int zipCode){
+        List<Customer> listCustomer = new ArrayList<>();
+        for (Customer customer : customers){
+            if (customer.getZipCode() == (zipCode)){
+                listCustomer.add(customer);
+            }
+        }
+        return listCustomer;
+    }
+
+    // Dado un nif y un ISBN, crea un préstamo para un socio. Crea un BookLend con la fecha actual.
+    //1º metodo para encontrar un libro por ISBN
+
+
+
+
+
 }
 
 

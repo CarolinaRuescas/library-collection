b0f8716b3115c1458fbf348777319ed7b06d2ddd
Carol <carol@b25>
2025-03-14 13:30:48 +0100

    Métodos terminados

--- src/main/java/org/ies/library/model/Library.java
+++ src/main/java/org/ies/library/model/Library.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.w3c.dom.ls.LSException;
 
+import java.time.LocalDate;
 import java.util.*;
 
 @Data
@@ -37,13 +38,82 @@ public class Library {
         return listCustomer;
     }
 
-    // Dado un nif y un ISBN, crea un préstamo para un socio. Crea un BookLend con la fecha actual.
-    //1º metodo para encontrar un libro por ISBN
+    // Dado un nif y un ISBN, crea un préstamo para un socio.
+    // Crea un BookLend con la fecha actual.
+
+    public void addBookLend(String nif, String isbn){
+        bookLends.add(
+                new BookLend(isbn, LocalDate.now(),nif)
+        );
+    }
+
+    // Dado un ISBN y un genero, elimina el genero del libro
+    // containsKey
+    // get
+    // remove
+
+    public boolean removeGenre (String isbn, String genre){
+        if(booksByIsbn.containsKey(isbn)){
+            Book book = booksByIsbn.get(isbn);
+            return book.getGenres().remove(genre);
+        }else{
+            return false;
+        }
+    }
+
+    // Dado un ISBN y un número de socio (no NIF), devuelve si el socio ha tomado
+    // prestado el libro o no (boolean).
+    // Foreach
+
+    public boolean customerHasLentBook (int customerNumber, String isbn){
+        var customer = findCustomer(customerNumber);
+        if (customer != null){
+            for (var bookLend : bookLends){
+                if (bookLend.getNif().equals(customer.getNif()) &&
+                        bookLend.getIsbn().equals(isbn)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //Metodo de apoyo para buscar un cliente con el numero
+
+    public Customer findCustomer(int customerNumber){
+        for(var customer : customers){
+            if(customer.getCustomerNumber()== customerNumber){
+                return customer;
+            }
+        }
+        return null;
+    }
 
 
+    // Dado un ISBN, devuelve los generos del libro. Si no existe el libro, devuleve null.
+    public Set<String> getBookGenres(String isbn){
+        if (booksByIsbn.containsKey(isbn)){
+            var book = booksByIsbn.get(isbn);
+            return book.getGenres();
+        }
+        return null;
+    }
 
-
-
+    // Dado un ISBN, devuelve todos los préstamos de ese libro. Si no existe el libro,
+    // devuelve null.
+    public List<BookLend> getBookLendsByIsbn(String isbn){
+        if (booksByIsbn.containsKey(isbn)){
+            List<BookLend> lends = new ArrayList<>();
+            for (var bookLend: bookLends){
+                if(bookLend.getIsbn().equals(isbn)){
+                    lends.add(bookLend);
+                }
+            }
+            return lends;
+        }else{
+            return null;
+        }
+    }
 }
 
 
